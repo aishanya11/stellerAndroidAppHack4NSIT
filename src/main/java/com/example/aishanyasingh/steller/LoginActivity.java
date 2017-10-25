edfd047f7631b310190fd4215e394a9c3de221a6
damakbajaj98 <damakbajaj98@gmail.com>
2017-10-25 19:23:11 +0530

    ilj,

--- src/main/java/com/example/aishanyasingh/steller/LoginActivity.java
+++ src/main/java/com/example/aishanyasingh/steller/LoginActivity.java
@@ -72,12 +72,12 @@ public class LoginActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
 
-                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // If sign in fails, display a message to the user that login has failed. If sign in succeeds
                         // the auth state listener will be notified and logic to handle the
                         // signed in user can be handled in the listener.
                         if (!task.isSuccessful()) {
                             Log.w(TAG, "signInWithEmail:failed", task.getException());
-                            Toast.makeText(LoginActivity.this, "failed",
+                            Toast.makeText(LoginActivity.this, "Login failed.",
                                     Toast.LENGTH_SHORT).show();
                         }else{
                             FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -104,7 +104,7 @@ public class LoginActivity extends AppCompatActivity {
         }
         else
         {
-            Toast.makeText(LoginActivity.this, "Incorrect email id or password",
+            Toast.makeText(LoginActivity.this, "Incorrect email id or password.",
                     Toast.LENGTH_SHORT).show();
         }
 
